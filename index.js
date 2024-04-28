e9a4e3f4ae2c31d272e3289d5a771bb964c1390d
ThinkMuk <b2000kang@gmail.com>
2024-04-28 18:25:28 +0900

    feat: Add content to aboutMe and revamp site-wide CSS

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ document.addEventListener('DOMContentLoaded', function () {
           // entry.target.id를 사용하여 각 태그의 ID에 따라 다른 메시지 출력
           switch (entry.target.id) {
             case 'aboutMe':
-              console.log('Element 1 is visible!');
+              //   console.log('Element 1 mount');
               aboutMe.style.fontWeight = 'bold';
               break;
             case 'MBTI':
-              console.log('Element 2 is visible!');
+              //   console.log('Element 2 mount');
               MBTI.style.fontWeight = 'bold';
               break;
             case 'favorite':
-              console.log('Element 3 is visible!');
+              //   console.log('Element 3 mount');
               favorite.style.fontWeight = 'bold';
               break;
           }
@@ -25,15 +25,15 @@ document.addEventListener('DOMContentLoaded', function () {
           // 태그가 더 이상 보이지 않을 때
           switch (entry.target.id) {
             case 'aboutMe':
-              console.log('Element 1 is no longer visible!');
+              //   console.log('Element 1 unmount');
               aboutMe.style.fontWeight = 'normal';
               break;
             case 'MBTI':
-              console.log('Element 2 is no longer visible!');
+              //   console.log('Element 2 unmount');
               MBTI.style.fontWeight = 'normal';
               break;
             case 'favorite':
-              console.log('Element 3 is no longer visible!');
+              //   console.log('Element 3 unmount');
               favorite.style.fontWeight = 'normal';
               break;
           }
